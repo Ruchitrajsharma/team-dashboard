36c0f9948fa86f2d85cc429c3631bb5b5c98f478
Ruchit Raj Sharma <143564090+Ruchitrajsharma@users.noreply.github.com>
2025-10-01 16:07:52 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Provider } from "react-redux"
+import { store } from "./redux/store"
+import Dashboard from "./pages/Dashboard"
+import "./App.css"
+
+function App() {
+
+
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Dashboard />
+      </div>
+    </Provider>
+  )
+}
+
+export default App
